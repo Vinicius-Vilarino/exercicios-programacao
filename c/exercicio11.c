31bb1cd5ee42ad7728a78dff0562b42621137d9c
Vinícius de Melo Vilarino <viniciusmelo688@gmail.com>
2026-01-02 06:30:49 -0300

    Add matrix initialization and input prompt
    Initialize a 3x3 matrix and prompt for user input.

---
+++ c/exercicio11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int linha, coluna;
+	int matriz[3][3];
+		for (linha = 0; linha < 3; linha++)
+		{
+			for (coluna = 0; coluna < 3; coluna++)
+		}
+		printf("Digitar os valores da matriz para: linha %d, coluna %d: ", linha + 1, coluna + 1);
+		scanf("%d", &matriz[linha][coluna]);
